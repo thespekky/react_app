6181b43780e2c740c200337a2c77533e4f3cf465
thespekky <bandras123456@gmail.com>
2024-02-09 16:15:45 +0100

    auth middleware elkészítése

--- api/Middlewares/auth.Middleware.js
+++ api/Middlewares/auth.Middleware.js
@@ -1,3 +1,32 @@
 const jwt = require("jsonwebtoken");
 const sequelize = require("../Models/dbModell");
-exports.auth = async (req, res, next) => {};
+const { QueryTypes } = require("sequelize");
+exports.auth = async (req, res, next) => {
+  try {
+    const authtoken = req.headers.authtoken
+      ? req.headers.authtoken
+      : req.headers.authorization.split(" ")[1];
+    if (!authtoken) {
+      res.send({ message: "Nincs token", success: false });
+    }
+    const data = jwt.verify(authtoken, process.env.ACCESS_TOKEN_KEY);
+    if (!data) {
+      res.send({ message: "Hibás token", success: false });
+    }
+    const user = await sequelize.query(
+      "SELECT ID, username, name, email,admin FROM users WHERE email=:Email",
+      {
+        replacements: { Email: data.email },
+        type: QueryTypes.SELECT,
+      }
+    );
+    if (!user) {
+      res.send({ message: "Nincs ilyen felhasználó", success: false });
+    }
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log(e);
+    res.send({ message: "Kritikus hiba" });
+  }
+};
