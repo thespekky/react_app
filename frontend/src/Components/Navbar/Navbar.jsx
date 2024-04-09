9419547a1d3372ecb56477a716d2652b17ea1e50
thespekky <bandras123456@gmail.com>
2024-04-09 19:00:57 +0200

    navbár mostmár responzív

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -3,16 +3,50 @@ import { useAuth } from "../AuthContext/AuthContext";
 import { useEffect } from "react";
 export default function Navbar() {
   const { isLoggedIn, logout } = useAuth();
+  let iisLoggedIn = true;
   return (
     <>
-      <nav className="navbar bg-stone-800 text-white flex justify-between items-stretch">
-        <Link to={"/"} className="text-5x1 title">
-          Fő oldal
-        </Link>
-        <ul className="p-0 m-0 list-none flex gap-3.5">
-          {isLoggedIn ? <CostumeLink to="/valami">valami</CostumeLink> : <></>}
+      <nav className="navbar bg-stone-800 text-white grid items-center ">
+        <div className="">
+          <Link to={"/"} className="text-5x1 title ">
+            Fő oldal
+          </Link>
+        </div>
+        <div className="">
+          <ul className="p-0 m-0 list-none flex max-lg:gap-0 gap-3.5">
+            {iisLoggedIn ? (
+              <CostumeLink to="/valami">valami</CostumeLink>
+            ) : (
+              <></>
+            )}
+          </ul>
+        </div>
+        <div className="">
+          <ul className="p-0 m-0 list-none row-span-3 col-span-1 flex max-lg:gap-0 gap-3.5">
+            {iisLoggedIn ? (
+              <CostumeLink to="/valami">valami</CostumeLink>
+            ) : (
+              <></>
+            )}
+          </ul>
+        </div>
+        <div className=" ">
+          <ul className="p-0 m-0 list-none max-lg:gap-0 gap-3.5">
+            {iisLoggedIn ? (
+              <CostumeLink to="/valami">valami</CostumeLink>
+            ) : (
+              <></>
+            )}
+          </ul>
+        </div>
+        <ul className="p-0 m-0 list-none flex  max-lg:gap-0 gap-3.5">
+          {iisLoggedIn ? <CostumeLink to="/valami">valami</CostumeLink> : <></>}
         </ul>
-        <ul className="p-0 m-0 list-none flex gap-3.5">
+        <ul className="p-0 m-0 list-none flex max-lg:gap-0 gap-3.5">
+          {iisLoggedIn ? <CostumeLink to="/valami">valami</CostumeLink> : <></>}
+        </ul>
+
+        <ul className="p-0 m-0 list-none max-lg:col-start-9 col-start-10">
           {!isLoggedIn ? (
             <CostumeLink to="/Reg">Regisztráció</CostumeLink>
           ) : (
