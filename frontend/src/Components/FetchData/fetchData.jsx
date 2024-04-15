60daecc194bc69eddbfdd6af848eab172008a36d
thespekky <bandras123456@gmail.com>
2024-04-15 19:07:40 +0200

    fetchData login

--- frontend/src/Components/FetchData/fetchData.jsx
+++ frontend/src/Components/FetchData/fetchData.jsx
@@ -22,6 +22,28 @@ export async function Register(path, body) {
     return { message: e.message };
   }
 }
+export async function Login(path, body) {
+  try {
+    const response = await fetch(
+      "http://localhost:" + import.meta.env.VITE_PORT + path,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    if (!response.ok) {
+      return { message: "Hiba a belépés során", success: false };
+    }
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+    return { message: e.message };
+  }
+}
 export async function GetAllData(path) {
   try {
     const response = await fetch(
